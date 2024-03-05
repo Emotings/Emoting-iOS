bf248aaa2db1c20d6bc121220fe1451dc4ecfd42
parkdoyeon1 <parkdoyeon3239@gmail.com>
2024-03-05 13:31:55 +0900

    ⚡️ :: ViewController & Reactor

---
+++ Projects/Presentation/Sources/Tabs/Friends/FriendsViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+public class FriendsViewController: BaseReactorViewController<FriendsReactor> {
+
+    public override func bindAction() {
+        view.backgroundColor = .black
+    }
+}
